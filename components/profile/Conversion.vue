f66559df84fb6f93dfad4cb8d12b36c1859d4b0a
Roger Waterson <aznrogerazn@gmail.com>
2019-09-18 23:37:36 +0800

    testing deployment

--- components/profile/Conversion.vue
+++ components/profile/Conversion.vue
@@ -5,6 +5,42 @@
     <div class="titular-row">
       <div>額度轉換</div>
     </div>
+    <div class="panel-row">
+      <div class="account-info">
+        <div class="tag">主帳戶</div>
+        <div class="value">9168.50</div>
+      </div>
+      <div class="dropdown">
+        <i class="fas fa-chevron-down"/>
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <div class="panel-row border">
+      <div class="conversion-input">
+        <span class="label">轉出金額</span>
+        <input type="number" v-model="amountToConvert"/>
+        <span class="all-in">全部轉出</span>
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <div class="panel-row">
+      <div class="account-info">
+        <div class="tag">主帳戶</div>
+        <div class="value">9168.50</div>
+      </div>
+      <div class="dropdown">
+        <i class="fas fa-chevron-down"/>
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <div class="panel-row">
+      <div class="confirm-button">
+        進行轉換
+      </div>
+    </div>
     <div
       class="modal"
       :class="isModalOpen ? 'active' : '' ">
@@ -26,6 +62,7 @@ export default {
   data() {
     return {
       isModalOpen: false,
+      amountToConvert: 0,
     };
   },
   computed: {},
@@ -39,6 +76,67 @@ export default {
   .titular-row
     padding-bottom: 1.5rem
     font-size: $size-3
+  .panel-row
+    display: flex
+    .account-info
+      flex-basis: 80%
+      .tag
+        font-size: $size-65
+        opacity: .6
+      .value
+        font-size: $size-3
+        font-family: 'Montserrat'
+        color: $complimentary-accent-2
+        padding: 5px 0 0 5px
+    .dropdown
+      flex-basis: 20%
+      display: flex
+      justify-content: center
+      align-items: center
+      i.fas
+        font-size: $size-7
+        opacity: .6
+        border: 1px solid $grey-light
+        width: 2rem
+        height: 2rem
+        line-height: 2rem
+        text-align: center
+    .conversion-input
+      position: relative
+      background: $grey-darker
+      border-radius: 3px
+      margin-top: -1rem
+      padding: 5px 5px 8px 5px
+      box-shadow: 0 0 9px 0 $grey-darker
+      .label
+        font-size: $size-7
+        padding: 2px 5px 3px 5px
+      input
+        font-size: $size-65
+        background: transparent
+        color: $grey-lighter
+        border: 0
+        outline: 0
+        width: 25vw
+      .all-in
+        background: $complimentary-accent-2
+        border-radius: 2px
+        padding: 2px 5px 3px 5px
+        color: $grey-dark
+        font-size: $size-7
+        vertical-align: middle
+    .confirm-button
+      background: $complimentary-accent-2
+      padding: 5px
+      color: $grey-dark
+      border-radius: 10px
+      letter-spacing: 2px
+      text-align: center
+      flex: 1
+      margin: .5rem 1.5rem
+    &.border
+      border-top: 1px solid $grey-light
+      justify-content: center
   &.style0
   &.style1
   &.style2
