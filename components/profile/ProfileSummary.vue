fe01400e80679e014f04f2d0a4585e41ca8ab708
Roger Waterson <aznrogerazn@gmail.com>
2019-09-18 19:46:02 +0800

    testing deployment

--- components/profile/ProfileSummary.vue
+++ components/profile/ProfileSummary.vue
@@ -12,6 +12,16 @@
       </div>
     </div>
     <br/>
+    <div class="coin-wrapper">
+      <img
+        class="coin back"
+        :class="showingCoins ? 'show' : '' "
+        src="/img/coin-back-c.png"/>
+      <img
+        class="coin front"
+        :class="showingCoins ? 'show' : '' "
+        src="/img/coin-front-c.png"/>
+    </div>
     <div class="balance-info">
       <div class="tag">中心餘額</div>
       <div class="value">{{ user.balance }}</div>
@@ -21,6 +31,27 @@
       <div class="refresh-now">
         <i class="fas fa-sync-alt"/>
         <span>立即刷新</span>
+      </div>
+    </div>
+    <br/>
+    <div class="shortcuts">
+      <div class="action">
+        <div class="icon green">
+          <i class="fas fa-sync"/>
+        </div>
+        轉換
+      </div>
+      <div class="action">
+        <div class="icon red">
+          <i class="fas fa-money-bill-alt"/>
+        </div>
+        提款
+      </div>
+      <div class="action">
+        <div class="icon blue">
+          <i class="fas fa-hand-holding-usd"/>
+        </div>
+        存款
       </div>
     </div>
   </div>
@@ -43,7 +74,13 @@ export default {
         balance: '9168.50',
         total_balance: '22168.50',
       },
+      showingCoins: false,
     };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showingCoins = true;
+    }, 300);
   },
 };
 </script>
@@ -65,7 +102,6 @@ export default {
   .balance-info
     position: relative
     padding: 1rem 0
-    border-top: 1px solid $grey-light
     border-bottom: 1px solid $grey-light
     .tag
       font-size: $size-65
@@ -83,6 +119,41 @@ export default {
       padding: 5px
       font-size: $size-65
       font-weight: 750
+  .coin-wrapper
+    border-top: 1px solid $grey-light
+    padding: 1.5rem 0
+    min-height: 120px
+    display: flex
+    justify-content: center
+    align-items: center
+    img.coin
+      height: 90px
+      &.front
+        transform: translate3d(-45px, 0, 0)
+        transition: .75s ease-in-out
+        &.show
+          transform: translate3d(-75px, 0, 0) rotate(-10deg)
+      &.back
+        transform: translate3d(45px, 0, 0)
+        transition: .75s ease-in-out
+        opacity: .3
+        &.show
+          transform: translate3d(45px, 0, 0)
+          opacity: 1
+  .shortcuts
+    display: flex
+    justify-content: space-around
+    .action
+      padding: 5px
+      .icon
+        font-size: $size-4
+        padding-bottom: 5px
+        &.green
+          color: #81A877
+        &.red
+          color: #D17867
+        &.blue
+          color: #5485B3
   &.style0
     .info-row
       img.icon
